acac78885a6a1bfb99be2134f00a7d1be2a976f2
kyleowen12345 <gakyleowen@gmail.com>
2021-02-19 10:13:43 +0800

    part3 finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,40 @@
-import { AuthProvider } from "../lib/auth"
-
+import { AuthProvider } from "@/lib/auth"
+import { ChakraProvider } from "@chakra-ui/react"
+import {Global,css} from '@emotion/react'
+import customTheme from "@/styles/theme"
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      {/* <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head> */}
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <AuthProvider>
+  return (
+    <ChakraProvider  theme={customTheme}>
+  <AuthProvider>
+    <GlobalStyle/>
     <Component {...pageProps}/>
   </AuthProvider>
+  </ChakraProvider >
+  )
+  
 }
 
 export default MyApp
