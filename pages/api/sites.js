1408719b80d48bea952687d1eae8dc1790d31881
kyleowen12345 <gakyleowen@gmail.com>
2021-02-25 14:34:04 +0800

    add logging and handle to api routes

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import {auth} from '@/lib/firebase-admin'
 import { getUserSites } from "@/lib/db-admin";
-
+import { logger, formatObjectKeys } from '@/utils/logger';
 export default async(req, res) => {
   try {
  const {uid}=await auth.verifyIdToken(req.headers.token)
@@ -9,6 +9,19 @@ export default async(req, res) => {
 
    res.status(200).json(sites)
   } catch (error) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      error.message
+    );
    res.status(500).json({error})
   }
  
